4752b68f52268f8a0ffa136169c4b10315a9c7f9
João Paulo <jp17paulo@terra.com.br>
2024-09-21 12:55:36 -0300

    feat: adicionando busca por cep na criação de clientes

--- src/app/cliente/clientecreate.tsx
+++ src/app/cliente/clientecreate.tsx
@@ -1,11 +1,10 @@
-//src/app/cliente/clientecreate.tsx
 "use client";
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { Cliente } from '../../models/cliente';
 import { SnackbarState } from '@/models/snackbarState';
 import InputMask from 'react-input-mask-next';
-import { useState } from 'react'; // Adicionar estado para controle dinâmico do CPF/CNPJ
+import { useState } from 'react'; 
 
 interface ClienteCreateFormProps {
   onSave: () => void;
@@ -15,13 +14,47 @@ interface ClienteCreateFormProps {
 
 export const ClienteCreateForm = ({ onSave, onClose, setSnackbar }: ClienteCreateFormProps) => {
   const { register, handleSubmit, reset, formState: { errors }  } = useForm<Cliente>();
-  
-  // Estado para controlar se é CPF ou CNPJ
-  const [isCNPJ, setIsCNPJ] = useState(false);
+
+  // Estados locais para mocar os campos de endereço
+  const [cep, setCep] = useState(''); 
+  const [logradouro, setLogradouro] = useState('');
+  const [complemento, setComplemento] = useState('');
+  const [bairro, setBairro] = useState('');
+  const [localidade, setLocalidade] = useState('');
+  const [uf, setUf] = useState('');
+
+  const [isCNPJ, setIsCNPJ] = useState(false); 
+
+  // Função para buscar endereço na API ViaCep
+  const buscarEnderecoViaCep = async (cep: string) => {
+    try {
+      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+      if (response.data.erro) {
+        setSnackbar(new SnackbarState('CEP não encontrado!', 'error', true));
+      } else {
+        // Preenche os campos de endereço com os dados retornados
+        setLogradouro(response.data.logradouro);
+        setComplemento(response.data.complemento);
+        setBairro(response.data.bairro);
+        setLocalidade(response.data.localidade);
+        setUf(response.data.uf);
+      }
+    } catch (error) {
+      setSnackbar(new SnackbarState('Erro ao buscar CEP!', 'error', true));
+    }
+  };
+
+  const handleCepChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const cepDigitado = e.target.value.replace(/\D/g, ''); 
+    setCep(cepDigitado); 
+    if (cepDigitado.length === 8) {
+      buscarEnderecoViaCep(cepDigitado); 
+    }
+  };
 
   const onSubmit = async (data: Cliente) => {
     try {
-      await axios.post('/api/Cliente', data);
+      await axios.post('/api/Cliente', data); 
       reset();
       onSave();
     } catch (error) {
@@ -29,89 +62,135 @@ export const ClienteCreateForm = ({ onSave, onClose, setSnackbar }: ClienteCreat
     }
   };
 
-  // Alternar entre CPF e CNPJ
   const toggleMask = () => {
-    setIsCNPJ(!isCNPJ); // Troca o valor entre CPF e CNPJ
+    setIsCNPJ(!isCNPJ); 
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="p-4">
-      <h2 className="text-xl font-bold mb-4">Novo Cliente</h2>
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
+      <form 
+        onSubmit={handleSubmit(onSubmit)} 
+        className="p-6 max-w-3xl w-full bg-white rounded-lg shadow-lg overflow-y-auto max-h-screen">
+        
+        <h2 className="text-xl font-bold mb-4 text-gray-800">Novo Cliente</h2>
 
-      <div className="mb-4">
-        <label className="block text-gray-700">Nome</label>
-        <input {...register('nome', { required: 'O nome é obrigatório' })} className="border rounded w-full py-2 px-3 mt-1" />
-        {errors.nome && <p className="text-red-500 text-sm">{errors.nome?.message}</p>}
-      </div>
-
-      <div className="mb-4">
-        <label className="block text-gray-700">Email</label>
-        <input {...register('email', { 
-            required: 'O e-mail é obrigatório',
-            pattern: {
-              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: 'E-mail inválido'
-            }
-          })
-        } className="border rounded w-full py-2 px-3 mt-1" />
-        {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
-      </div>
-
-      {/* Campo CPF/CNPJ com a máscara dinâmica */}
-      <div className="mb-4">
-        <label className="block text-gray-700">CPF/CNPJ</label>
-        <div className="flex">
-          <InputMask
-            {...register('cpfCnpj', { required: 'CPF/CNPJ é obrigatório'  })}
-            mask={isCNPJ ? '99.999.999/9999-99' : '999.999.999-99'}
-            className="border rounded w-full py-2 px-3 mt-1"
-            placeholder={isCNPJ ? 'CNPJ' : 'CPF'}
+        {/* Campo Nome */}
+        <div className="mb-4">
+          <label className="block text-gray-800">Nome</label>
+          <input 
+            {...register('nome', { required: 'O nome é obrigatório' })} 
+            className="border rounded w-full py-2 px-3 mt-1 text-gray-800" 
           />
-          <button
-            type="button"
-            onClick={toggleMask}
-            className="ml-2 py-2 px-4 bg-blue-500 text-white rounded"
-          >
-            {isCNPJ ? 'Usar CPF' : 'Usar CNPJ'}
+          {errors.nome && <p className="text-red-500 text-sm">{errors.nome?.message}</p>}
+        </div>
+
+        {/* Campo Email */}
+        <div className="mb-4">
+          <label className="block text-gray-800">Email</label>
+          <input 
+            {...register('email', { 
+              required: 'O e-mail é obrigatório',
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: 'E-mail inválido'
+              }
+            })}
+            className="border rounded w-full py-2 px-3 mt-1 text-gray-800" 
+          />
+          {errors.email && <p className="text-red-500 text-sm">{errors.email?.message}</p>}
+        </div>
+
+        {/* Campo CPF/CNPJ */}
+        <div className="mb-4">
+          <label className="block text-gray-800">CPF/CNPJ</label>
+          <div className="flex">
+            <InputMask
+              {...register('cpfCnpj', { required: 'CPF/CNPJ é obrigatório' })}
+              mask={isCNPJ ? '99.999.999/9999-99' : '999.999.999-99'}
+              className="border rounded w-full py-2 px-3 mt-1 text-gray-800"
+              placeholder={isCNPJ ? 'CNPJ' : 'CPF'}
+            />
+            <button
+              type="button"
+              onClick={toggleMask}
+              className="ml-2 py-2 px-4 bg-blue-500 text-white rounded"
+            >
+              {isCNPJ ? 'Usar CPF' : 'Usar CNPJ'}
+            </button>
+          </div>
+          {errors.cpfCnpj && <p className="text-red-500 text-sm">{errors.cpfCnpj?.message}</p>}
+        </div>
+
+        {/* Campo CEP */}
+        <div className="mb-4">
+          <label className="block text-gray-800">CEP</label>
+          <InputMask
+            value={cep} 
+            mask="99999-999"
+            className="border rounded w-full py-2 px-3 mt-1 text-gray-800"
+            onChange={handleCepChange} 
+          />
+        </div>
+
+        {/* Campo Logradouro (Rua) */}
+        <div className="mb-4">
+          <label className="block text-gray-800">Rua (Logradouro)</label>
+          <input 
+            value={logradouro} 
+            className="border rounded w-full py-2 px-3 mt-1 text-gray-800" 
+            readOnly 
+          />
+        </div>
+
+        {/* Campo Complemento */}
+        <div className="mb-4">
+          <label className="block text-gray-800">Complemento</label>
+          <input 
+            value={complemento} 
+            className="border rounded w-full py-2 px-3 mt-1 text-gray-800" 
+            readOnly 
+          />
+        </div>
+
+        {/* Campo Bairro */}
+        <div className="mb-4">
+          <label className="block text-gray-800">Bairro</label>
+          <input 
+            value={bairro} 
+            className="border rounded w-full py-2 px-3 mt-1 text-gray-800" 
+            readOnly 
+          />
+        </div>
+
+        {/* Campo Cidade */}
+        <div className="mb-4">
+          <label className="block text-gray-800">Cidade</label>
+          <input 
+            value={localidade} 
+            className="border rounded w-full py-2 px-3 mt-1 text-gray-800" 
+            readOnly 
+          />
+        </div>
+
+        {/* Campo UF */}
+        <div className="mb-4">
+          <label className="block text-gray-800">UF</label>
+          <input 
+            value={uf} 
+            className="border rounded w-full py-2 px-3 mt-1 text-gray-800" 
+            readOnly 
+          />
+        </div>
+
+        <div className="flex justify-end">
+          <button type="button" onClick={onClose} className="mr-2 py-2 px-4 rounded bg-gray-500 text-white">
+            Cancelar
+          </button>
+          <button type="submit" className="py-2 px-4 rounded bg-blue-500 text-white">
+            Salvar
           </button>
         </div>
-        {errors.cpfCnpj && <p className="text-red-500 text-sm">{errors.cpfCnpj?.message}</p>}
-      </div>
-
-      <div className="mb-4">
-        <label className="block text-gray-700">Endereço</label>
-        <input {...register('endereco',{required: 'O endereço é obrigatório'})} className="border rounded w-full py-2 px-3 mt-1" />
-        {errors.endereco && <p className="text-red-500 text-sm">{errors.endereco?.message}</p>}
-      </div>
-
-      <div className="mb-4">
-        <label className="block text-gray-700">Telefone</label>
-        <InputMask
-           {...register('telefone', { required: 'O telefone é obrigatório' })}
-          mask="(99) 99999-9999"
-          className="border rounded w-full py-2 px-3 mt-1"
-        />
-          {errors.telefone && <p className="text-red-500 text-sm">{errors.telefone?.message}</p>}
-      </div>
-
-      <div className="mb-4">
-        <label className="block text-gray-700">Data de Cadastro</label>
-        <input
-          {...register('dataCadastro', { required: 'A data de cadastro é obrigatória' })}
-          type="date"
-          className="border rounded w-full py-2 px-3 mt-1"
-        />
-         {errors.dataCadastro && <p className="text-red-500 text-sm">{errors.dataCadastro?.message}</p>}
-      </div>
-
-      <div className="flex justify-end">
-        <button type="button" onClick={onClose} className="mr-2 py-2 px-4 rounded bg-gray-500 text-white">
-          Cancelar
-        </button>
-        <button type="submit" className="py-2 px-4 rounded bg-blue-500 text-white">
-          Salvar
-        </button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
