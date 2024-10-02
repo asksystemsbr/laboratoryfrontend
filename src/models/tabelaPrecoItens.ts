b8dd144180bb84c2c5a33e08e5e3b5f52aac4e69
alex_kaam <contato@asksystems.com.br>
2024-10-02 18:12:30 -0300

    + Price table

--- src/models/tabelaPrecoItens.ts
+++ src/models/tabelaPrecoItens.ts
@@ -1,11 +1,12 @@
-// src/models/tabelaPreco.ts
-export interface TabelaPreco {
+//src/models/tabelaPrecoItens.ts
+export interface TabelaPrecoItens {
     id?: number;    
     tabelaPrecoId: number;
-    exameId: number;
+    exameId: number ;
     valor: number;
     custoOperacional: number;
     custoHorario: number;
     filme: number;
     codigoArnb: string;
+    nomeExame?: string; // Novo campo para o nome do exame
   }
