f29d3638f3ff6bdf410577eed96dc405d84d943c
alex_kaam <contato@asksystems.com.br>
2024-10-03 12:09:10 -0300

    -Fix page cadastros after merge

--- src/app/cadastros/page.tsx
+++ src/app/cadastros/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import CadastroMenu from '../../components/CadastroMenu';
-import React from 'react';
 import Menu from '../../components/menu';
 
 export default function CadastrosPage() {
