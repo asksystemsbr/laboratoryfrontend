9ddfda3aa17b04d819a50e7da54af1f9d26cf3f7
João Paulo <jp17paulo@terra.com.br>
2024-10-03 21:57:02 -0300

    feat: atualiza páginas relacionadas a exames (especialidade, método, modalidade, recipiente, rotina, setor)

--- src/app/especialidade/page.tsx
+++ src/app/especialidade/page.tsx
@@ -1,46 +1,59 @@
-//src/app/especialidade/page.tsx
 "use client";
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { EspecialidadeCreateForm } from './especialidadecreate';
 import { EspecialidadeEditForm } from './especialidadeedit';
 import { Snackbar } from '../snackbar';
 import Modal from 'react-modal';
 import axios from 'axios';
 import { Especialidade } from '../../models/especialidade';
-import { SnackbarState } from '../../models/snackbarState'; // Importa a classe
-import Menu from '../../components/menu'; // Importa o menu
-import ConfirmationModal from '../../components/confirmationModal'; // Importa a modal genérica
-import React from 'react';
+import { SnackbarState } from '../../models/snackbarState';
+import Menu from '../../components/menu';
+import ConfirmationModal from '../../components/confirmationModal';
 
 Modal.setAppElement('#__next');
 
-export default function ItemsList() {
-  const [items, setItems] = useState<Especialidade[]>([]);
+export default function EspecialidadeList() {
+  const [especialidades, setEspecialidades] = useState<Especialidade[]>([]);
   const [filtered, setFiltered] = useState<Especialidade[]>([]);
-  const [searchTerm, setSearchTerm] = useState(''); // Estado para o termo de busca
+  const [searchTerm, setSearchTerm] = useState('');
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
-  const [editingItem, setEditingItem] = useState<Especialidade | null>(null);
-  const [snackbar, setSnackbar] = useState(new SnackbarState()); // Instância de SnackbarState
-  const [progress, setProgress] = useState(100); // Para a barra de progresso
-  const [deleteConfirmOpen, setDeleteConfirmOpen] = useState(false); // Controla o modal de confirmação de exclusão
-  const [itemToDelete, setItemToDelete] = useState<number | null>(null); 
+  const [editingEspecialidade, setEditingEspecialidade] = useState<Especialidade | null>(null);
+  const [snackbar, setSnackbar] = useState(new SnackbarState());
+  const [deleteConfirmOpen, setDeleteConfirmOpen] = useState(false);
+  const [especialidadeToDelete, setEspecialidadeToDelete] = useState<number | null>(null);
+  const [progress, setProgress] = useState(100); // Barra de progresso para o snackbar
+  const recordsPerPage = 10;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [dropdownVisible, setDropdownVisible] = useState<{ [key: number]: boolean }>({});
+
+  // Função para carregar especialidades
+  const loadEspecialidades = async () => {
+    try {
+      const response = await axios.get('/api/Especialidade');
+      setEspecialidades(response.data);
+      setFiltered(response.data);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      setSnackbar(new SnackbarState('Erro ao carregar especialidades!', 'error', true));
+    }
+  };
 
   useEffect(() => {
-    loadItems();
+    loadEspecialidades();
   }, []);
 
-  // Timer para fechar o snackbar automaticamente
+  // Função para controle do Snackbar
   useEffect(() => {
     if (snackbar.show) {
       const interval = setInterval(() => {
-        setProgress((prev) => (prev > 0 ? prev - 1 : 0)); // Reduz progressivamente
+        setProgress((prev) => (prev > 0 ? prev - 1 : 0));
       }, 50);
 
       const timer = setTimeout(() => {
-        snackbar.hideSnackbar(); // Esconde o Snackbar
-        setSnackbar(new SnackbarState()); // Atualiza para uma nova instância
-        setProgress(100); // Reset progress
+        snackbar.hideSnackbar();
+        setSnackbar(new SnackbarState());
+        setProgress(100); // Reset progresso
       }, 5000);
 
       return () => {
@@ -50,161 +63,223 @@ export default function ItemsList() {
     }
   }, [snackbar]);
 
-  useEffect(() => {
-    const filtered = items.filter((item) => {
-      // Obtenha todas as chaves (campos) do objeto `cliente`
-        return Object.values(item).some((value) => 
-        value && value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+  const applySearch = useCallback(() => {
+    const searchTermLower = searchTerm.toLowerCase();
+    const filteredEspecialidades = especialidades.filter((especialidade) =>
+      Object.values(especialidade).some((value) =>
+        String(value).toLowerCase().includes(searchTermLower)
+      )
     );
-    });
-    setFiltered(filtered);
-  }, [searchTerm, items]);
+    setFiltered(filteredEspecialidades);
+  }, [searchTerm, especialidades]);
 
-  // Função para carregar os dados
-  const loadItems = async () => {
-    try {
-      const response = await axios.get('/api/Especialidade');
-      setItems(response.data);
-      setFiltered(response.data); 
-    } catch (error) {
-      console.log(error);
-      setSnackbar(new SnackbarState('Erro ao carregar dados!', 'error', true));
-    }
+  useEffect(() => {
+    applySearch();
+  }, [searchTerm, especialidades, applySearch]);
+
+  const handleSave = () => {
+    setModalIsOpen(false);
+    setSnackbar(new SnackbarState('Registro salvo com sucesso!', 'success', true));
+    loadEspecialidades();
   };
 
   const handleDelete = async () => {
-    if (itemToDelete !== null) {
+    if (especialidadeToDelete !== null) {
       try {
-        await axios.delete(`/api/Especialidade/${itemToDelete}`);
-        setSnackbar(new SnackbarState('Registro excluído com sucesso!', 'success', true));
-        loadItems();
-        closeDeleteConfirm(); // Fechar o modal de confirmação após a exclusão
+        await axios.delete(`/api/Especialidade/${especialidadeToDelete}`);
+        setSnackbar(new SnackbarState('Especialidade excluída com sucesso!', 'success', true));
+        setDeleteConfirmOpen(false);
+        loadEspecialidades();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       } catch (error) {
-        console.log(error);
-        setSnackbar(new SnackbarState('Erro ao excluir registro!', 'error', true));
+        setSnackbar(new SnackbarState('Erro ao excluir especialidade!', 'error', true));
       }
     }
   };
 
-    // Abrir modal de confirmação de exclusão
-    const openDeleteConfirm = (id: number) => {
-        setItemToDelete(id);
-        setDeleteConfirmOpen(true);
-      };
-    
-      // Fechar modal de confirmação
-      const closeDeleteConfirm = () => {
-        setItemToDelete(null);
-        setDeleteConfirmOpen(false);
-      };
-
-  // Fechar modal e exibir snackbar ao salvar
-  const handleSave = () => {
-    setModalIsOpen(false);
-    setSnackbar(new SnackbarState('Registro salvo com sucesso!', 'success', true));
-    loadItems();
-  };
-
-  // Abrir modal para criar
-  const handleCreate = () => {
-    setIsEditing(false);
-    setModalIsOpen(true);
-  };
-
-  // Abrir modal para editar
-  const handleEdit = (item: Especialidade) => {
-    setEditingItem(item);
+  const handleEdit = (especialidade: Especialidade) => {
+    setEditingEspecialidade(especialidade);
     setIsEditing(true);
     setModalIsOpen(true);
+    setDropdownVisible({});
   };
 
-  return (
-    <div className="flex h-screen">
-      {/* Inclui o menu no lado esquerdo */}
-      <Menu />
+  const handleNewEspecialidade = () => {
+    setIsEditing(false);
+    setEditingEspecialidade(null);
+    setModalIsOpen(true);
+  };
 
+  const toggleDropdown = (id: number) => {
+    setDropdownVisible((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+
+  const handleDeleteConfirmation = (id: number) => {
+    setEspecialidadeToDelete(id);
+    setDeleteConfirmOpen(true);
+    setDropdownVisible({});
+  };
+
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  const currentEspecialidades = filtered.slice(indexOfFirstRecord, indexOfLastRecord);
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    const isClickInside = (event.target as HTMLElement).closest('.dropdown-actions');
+    if (!isClickInside) {
+      setDropdownVisible({});
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+    return () => document.removeEventListener('click', handleClickOutside);
+  }, []);
+
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <Menu />
       <div className="container mx-auto p-8">
         <div className="flex justify-between items-center mb-6">
-          <h1 className="text-2xl font-bold">Especialidades</h1>
+          <h1 className="text-3xl font-bold text-gray-800">Especialidades</h1>
           <button
-            onClick={handleCreate}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleNewEspecialidade}
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-6 rounded-lg shadow-md transition-all duration-300"
           >
             Nova Especialidade
           </button>
         </div>
 
         {/* Campo de busca */}
-        <div className="mb-4">
+        <div className="mb-4 relative">
           <input
             type="text"
             placeholder="Buscar"
             value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)} // Atualiza o termo de busca
-            className="border border-gray-300 rounded-lg py-2 px-4 w-full"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="border border-gray-300 rounded-lg py-2 px-4 w-full text-gray-700 focus:outline-none focus:ring-2 focus:ring-green-500"
           />
+          <span className="absolute right-4 top-2.5 text-gray-400">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path
+                fillRule="evenodd"
+                d="M12.9 14.32a8 8 0 111.42-1.42l4.28 4.29a1 1 0 11-1.42 1.42l-4.28-4.29zM8 14a6 6 0 100-12 6 6 0 000 12z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </span>
         </div>
 
-        {/* Tabela de listagem */}
-        <table className="min-w-full bg-white border border-gray-200">
+        {/* Tabela */}
+        <table className="min-w-full bg-white border border-gray-300 rounded-lg">
           <thead>
-            <tr>
-              <th className="py-2 px-4 text-left">Descrição</th>
-              <th className="py-2 px-4 text-left">Editar</th>
-              <th className="py-2 px-4 text-left">Excluir</th>
+            <tr className="bg-gray-50">
+              <th className="py-3 px-6 text-left text-sm font-semibold text-gray-600 border-b">Descrição</th>
+              <th className="py-3 px-6 text-left text-sm font-semibold text-gray-600 border-b">Ações</th>
             </tr>
           </thead>
           <tbody>
-            {filtered.map((item) => (
-              <tr key={item.id} className="border-t border-gray-200">
-                <td className="py-2 px-4 text-left">{item.descricao}</td>
-                <td className="py-2 px-4 text-left">
+            {currentEspecialidades.map((especialidade) => (
+              <tr key={especialidade.id} className="border-t border-gray-300 hover:bg-gray-100 transition">
+                <td className="py-3 px-6 text-left text-sm text-gray-800">{especialidade.descricao}</td>
+                <td className="py-3 px-6 text-left relative dropdown-actions">
                   <button
-                    className="text-yellow-500 hover:text-yellow-700 mr-2"
-                    onClick={() => handleEdit(item)}
+                    onClick={() => toggleDropdown(especialidade.id!)}
+                    className="bg-gray-200 hover:bg-gray-300 text-gray-700 font-semibold py-1 px-4 rounded-lg shadow-sm transition-all duration-200"
                   >
-                    Editar
+                    Ações
                   </button>
-                </td>
-                <td className="py-2 px-4">
-                  <button
-                    className="text-red-500 hover:text-red-700"
-                    onClick={() => openDeleteConfirm(item.id!)} // Usa a função de abrir o modal de confirmação
-                  >
-                    Excluir
-                  </button>
+                  {dropdownVisible[especialidade.id!] && (
+                    <div className="absolute mt-2 w-32 bg-white border rounded-lg shadow-lg z-10">
+                      <ul className="py-1">
+                        <li
+                          className="px-4 py-2 text-sm text-gray-800 hover:bg-gray-100 cursor-pointer"
+                          onClick={() => handleEdit(especialidade)}
+                        >
+                          Editar
+                        </li>
+                        <li
+                          className="px-4 py-2 text-sm text-red-500 hover:bg-gray-100 cursor-pointer"
+                          onClick={() => handleDeleteConfirmation(especialidade.id!)}
+                        >
+                          Excluir
+                        </li>
+                      </ul>
+                    </div>
+                  )}
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
 
-        {/* Modal para criar ou editar */}
+        {/* Paginação */}
+        <div className="flex justify-between mt-4">
+          <button
+            onClick={() => paginate(currentPage - 1)}
+            disabled={currentPage === 1}
+            className="bg-gray-200 hover:bg-gray-300 text-gray-600 font-semibold py-2 px-4 rounded-lg flex items-center justify-center shadow-sm transition-all duration-200"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+            </svg>
+            Anterior
+          </button>
+
+          <span className="text-gray-600">Página {currentPage}</span>
+
+          <button
+            onClick={() => paginate(currentPage + 1)}
+            disabled={currentPage === Math.ceil(filtered.length / recordsPerPage)}
+            className="bg-gray-200 hover:bg-gray-300 text-gray-600 font-semibold py-2 px-4 rounded-lg flex items-center justify-center shadow-sm transition-all duration-200"
+          >
+            Próxima
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+            </svg>
+          </button>
+        </div>
+
+        {/* Modal de criação e edição */}
         <Modal
           isOpen={modalIsOpen}
           onRequestClose={() => setModalIsOpen(false)}
-          className="bg-white p-6 max-w-xl mx-auto rounded-lg shadow-lg w-full" // Tornando o modal maior
-          overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center" // Classe para o overlay
+          className="bg-white p-6 max-w-xl mx-auto rounded-lg shadow-lg w-full"
+          overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
         >
           {isEditing ? (
-            <EspecialidadeEditForm especialidade={editingItem!} onSave={handleSave} onClose={() => setModalIsOpen(false)} setSnackbar={setSnackbar} />
+            <EspecialidadeEditForm
+              especialidade={editingEspecialidade!}
+              onSave={handleSave}
+              onClose={() => setModalIsOpen(false)}
+              setSnackbar={setSnackbar}
+            />
           ) : (
-            <EspecialidadeCreateForm onSave={handleSave} onClose={() => setModalIsOpen(false)} setSnackbar={setSnackbar} />
+            <EspecialidadeCreateForm
+              onSave={handleSave}
+              onClose={() => setModalIsOpen(false)}
+              setSnackbar={setSnackbar}
+            />
           )}
         </Modal>
 
-        {/* Modal de confirmação para exclusão */}
+        {/* Modal de confirmação de exclusão */}
         <ConfirmationModal
           isOpen={deleteConfirmOpen}
           title="Confirmação de Exclusão"
-          message="Tem certeza de que deseja excluir este registro? Esta ação não pode ser desfeita."
-          onConfirm={handleDelete} // Não passa parâmetros aqui
-          onCancel={closeDeleteConfirm} // Fecha o modal
+          message="Tem certeza de que deseja excluir esta especialidade? Esta ação não pode ser desfeita."
+          onConfirm={handleDelete}
+          onCancel={() => setDeleteConfirmOpen(false)}
           confirmText="Excluir"
           cancelText="Cancelar"
         />
 
-        {/* Snackbar de feedback */}
+        {/* Snackbar */}
         {snackbar.show && (
           <Snackbar message={snackbar.message} type={snackbar.type} progress={progress} />
         )}
